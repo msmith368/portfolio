60015659c3c9a7189918ebdbaedc81df133613e0
Matthew Smith <94403504+msmith368@users.noreply.github.com>
2024-10-01 19:35:38 -0400

    Update Contact.jsx
    fixed resume link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
         <div className="flex flex-col mb-10 mx-auto">
             <div className="flex justify-center items-center">
                 <a
-                    href="/assets/resume_2024.pdf"
+                    href="/portfolio/assets/resume_2024.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-orange-400 to-yellow-500 drop-shadow-md hover:stroke-white"
